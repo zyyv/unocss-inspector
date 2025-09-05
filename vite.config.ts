ff4f5cfc415c95ac2cc74c56f63b6bc8d72601a8
zhangmo8 <wegi866@gmail.com>
2025-09-05 14:59:22 +0800

    revert: vite.config & package

--- vite.config.ts
+++ vite.config.ts
@@ -8,43 +8,4 @@ export default defineConfig({
   define: {
     'process.env.BABEL_TYPES_8_BREAKING': 'false',
   },
-  build: {
-    rollupOptions: {
-      external: [
-        'tinyexec',
-        '@antfu/install-pkg',
-        'local-pkg',
-        'mlly',
-        'node:fs',
-        'node:fs/promises',
-        'node:path',
-        'node:process',
-        'node:module',
-        'node:util',
-        'node:child_process',
-        'node:os',
-        'node:url',
-        'node:assert',
-        'node:v8',
-        'fs',
-        'path',
-        'process',
-        'module',
-        'util',
-        'child_process',
-        'os',
-        'url',
-        'assert',
-        'v8',
-      ],
-    },
-  },
-  optimizeDeps: {
-    exclude: [
-      'tinyexec',
-      '@antfu/install-pkg',
-      'local-pkg',
-      'mlly',
-    ],
-  },
 })
