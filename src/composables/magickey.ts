e3b9356551db85b9e0776ba769105a5e1baa96ba
xiaomo <wegi866@gmail.com>
2025-09-05 11:10:16 +0800

    feat: keydown esc to exit (#2)

--- src/composables/magickey.ts
+++ src/composables/magickey.ts
@@ -1,14 +1,21 @@
 import { useMagicKeys } from '@vueuse/core'
 import { watch } from 'vue'
 
-export function useMagicKey(action: () => void) {
+export function useMagicKey(action: () => void, exit: () => void) {
   const keys = useMagicKeys()
   const cmdU = keys['cmd+u']
   const ctrlU = keys['ctrl+u']
+  const escape = keys.escape
 
   watch([cmdU, ctrlU], ([cmdUPressed, ctrlUPressed]) => {
     if (cmdUPressed || ctrlUPressed) {
       action()
+    }
+  })
+
+  watch(escape, (escPressed) => {
+    if (escPressed) {
+      exit()
     }
   })
 }
