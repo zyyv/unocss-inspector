107da45fa2cc0443097b2c350f50e7aab9128692
Chris <hizyyv@gmail.com>
2025-09-05 14:03:50 +0800

    chore: update export

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import type { App } from 'vue'
+import FormControl from './components/basic/FormControl.vue'
+import FormControlGroup from './components/basic/FormControlGroup.vue'
 import Inspector from './Inspector.vue'
 
-export { Inspector }
+export { FormControl, FormControlGroup, Inspector }
 
 export default {
   install: (app: App) => {
     app.component('Inspector', Inspector)
+    app.component('FormControl', FormControl)
+    app.component('FormControlGroup', FormControlGroup)
   },
 }
