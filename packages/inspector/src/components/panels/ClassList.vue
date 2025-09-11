4a418ff6f4a4f6f445dc66ae310e7f20c5e0ed69
Chris <hizyyv@gmail.com>
2025-09-11 17:39:27 +0800

    feat: add empty to display

--- packages/inspector/src/components/panels/ClassList.vue
+++ packages/inspector/src/components/panels/ClassList.vue
@@ -3,6 +3,7 @@ import { useAttributes } from '../../composables/attributes'
 import { useClassList } from '../../composables/classList'
 import AttributesSection from '../sections/AttributesSection.vue'
 import ClassListSection from '../sections/ClassListSection.vue'
+import Empty from '../sections/Empty.vue'
 
 const { displayClasses, classList } = useClassList()
 const { attributes, updateAttribute } = useAttributes()
@@ -10,17 +11,20 @@ const { attributes, updateAttribute } = useAttributes()
 
 <template>
   <div p-3>
-    <ClassListSection
-      :display-classes="displayClasses"
-      :class-list="classList"
-      @update:class-list="(newList) => classList = newList"
-    />
+    <Empty v-if="displayClasses.length === 0 && attributes.size === 0" />
+    <template v-else>
+      <ClassListSection
+        :display-classes="displayClasses"
+        :class-list="classList"
+        @update:class-list="(newList) => classList = newList"
+      />
 
-    <div v-if="attributes.size > 0 && displayClasses.length > 0" my-2 divided />
+      <div v-if="attributes.size > 0 && displayClasses.length > 0" my-2 divided />
 
-    <AttributesSection
-      :attributes="attributes"
-      @update-attribute="updateAttribute"
-    />
+      <AttributesSection
+        :attributes="attributes"
+        @update-attribute="updateAttribute"
+      />
+    </template>
   </div>
 </template>
